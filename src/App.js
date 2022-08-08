5525089a6dc23389c7141c2bac1b9f05cb7f7515
andrustn85 <rustandres@gmail.com>
2022-08-08 14:31:59 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [fullName, setFullname] = useState({
+  fName: "",
+  lName: "",
+  email: ""
+});
+
+function handleChange(event) {
+const { value, name } = event.target;
+
+setFullname(prevValue => {
+  if (name === "fName") {
+    return {
+      fName: value,
+   lName: fullName.lName,
+   email: fullName.email
+    }; 
+  } else if (name === "lName") {  
+    return {   
+       fName: fullName.fName,
+      lName: value,
+      email: fullName.email
+    };
+    } else return {   
+         fName: fullName.fName,
+        lName: fullName.lName,
+        email: value
+  }
+})
+
+   
+  }
+
+
+return (
+  <div className="container">
+    <h1>
+      Hello {fullName.fName} {fullName.lName}
+    </h1>
+    <p> {fullName.email} </p>
+    <form>
+      <input
+        onChange={handleChange}
+        name="fName"
+        placeholder="First Name"
+        value={fullName.fName}
+      />
+      <input
+        onChange={handleChange}
+        name="lName"
+        placeholder="Last Name"
+        value={fullName.lName}
+      />
+<input 
+onChange={handleChange}
+name="email" 
+placeholder="Email" 
+value={fullName.email}
+/>
+        <button> Submit </button>
+      </form>
     </div>
   );
 }
